a891d6f052790e4e527c4d52e391361050b9786c
TeoCode <58634700+TeoCode@users.noreply.github.com>
2019-12-07 17:49:55 +0100

    Create TheFortniter.bat

---
+++ TheFortniter.bat
@@ -0,0 +1,49 @@
+@echo off
+color 0f
+title The Fortniter
+echo Ciao Fortniter, benvenuto in questo gioco dimostrativo.
+echo.
+echo Vuoi sapere come funziona? [si/no]
+set /p risp=Tu:
+if %risp%==si echo Semplice: vivrai quello che vive ogni giorno Un Vero Fortniter
+if %risp%==no echo ok allora cominciamo subito!
+echo.
+echo Quando un Fortniter si sveglia controlla le nuove "patch" del suo amato Fortnite.
+echo A prescindere, poi, apre Fortnite e comincia a fare una partita.
+echo.
+echo Arriva il tuo momento: vinci la partita o perdila [vinci/perdi]
+set /p risp=Tu:
+if %risp%==vinci echo Congratulazione hai effettuato la Vittoria Reale!
+if %risp%==perdi echo Anche quando un Fortniter vuole perdere, vince sempre quindi... Vittora Reale!
+echo.
+echo Attenzione! nuovo evento cancer..Bellissimo in arrivo: Il Buco Nero!
+echo Come ogni Fortniter devi assolutamente assisterci, ma vuoi veramente sprecare un giorno a vedere un inutile buco nero che non fa niente? [si/no]
+set /p risp=Tu:
+if %risp%==si echo Il tuo instinto da Fortniter ti spinge a guardarlo tutto il tempo, non puoi farci niente... La tua vita sta diventando sempre maggiormente insignificante...
+if %risp%==no echo Il tuo instinto da Fortniter ti spinge a guardarlo tutto il tempo anche se non vuoi, non puoi farci niente... La tua vita sta diventando sempre maggiormente insignificante...
+echo.
+echo Il gioco sta consumando piano piano il tuo cervello, bisogna smettere, devi assolutamente smettere!
+echo Questo gioco sta degenerando, doveva essere solo una simulazione della vita di un Fortniter invece sta corrompendo la mente anche tua, che stai assistendo alla dimostrazione.
+echo.
+echo SYSTEM ERROR! REBOOTING...
+echo SYSTEM ERROR! REBOOTING...
+echo SYSTEM ERROR! REBOOTING...
+echo SYSTEM ERROR! REBOOTING...
+echo.
+echo Epic Games: Il nostro piano ha funzionato Muahahahaha!!!!
+echo Epic Games: Stupido giocatore, Fortnite rimane per sempre nel tuo cervello e non puoi privartene...
+echo Epic Games: L'unico modo sarebbe azzeccare la combinazione segrata, tu ce la farai? [1234z]
+set /p risp=Tu:
+if %risp%==1234z echo Epic Games: oh fuck, in questo gioco ci sono le possibili risposte, non ci avevamo pensato.
+echo Epic Games: anche se il giocatore ha sbagliato la combinazione Windows Batch manda avanti i comandi, la nostra sconfitta era segnata.
+echo.
+echo ...
+echo ...
+echo ...
+echo Epic Games: il nostro potere sta svane....POOF
+echo.
+echo Congratulazione hai vinto, Hai scomfitto la peste Epic Games!
+echo Era tutto un test per capire i veri potenzialid ei giocatori che giocano a Fortnite
+echo Premi un tasto per uscire
+echo Epic Games non mi piacchiare t.v.b
+pause > nul
